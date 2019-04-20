fa016dffeb775d52cbaf594a744d55b185ac5056
Pogostick <49816096+PogoCat@users.noreply.github.com>
2019-04-20 17:38:59 +0200

    Add files via upload

---
+++ underagetest/src/WUI/Kinder.java
@@ -0,0 +1,29 @@
+package WUI;
+import java.util.Scanner;
+
+public class Kinder {
+	
+	@SuppressWarnings("resource")
+	public static void main (String [] args) {
+		
+	double alter;
+	Scanner scannerVariable = new Scanner(System.in);
+		
+		System.out.println("Wie alt ist das Mädchen ?");
+		alter=scannerVariable.nextDouble();
+		
+		if (alter<18) {
+		 System.out.println("FBI OPEN UP");
+		}
+		 
+		 else
+		 System.out.println("klär dir du kleiner Hentai boy");
+		}
+		 
+		
+		
+	}
+		
+
+
+	
